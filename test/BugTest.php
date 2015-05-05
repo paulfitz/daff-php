acfe635102a089796f456f1caffa379e5c9df0df
sp-lorenzo-arribas <lorenzo.arribas@socialpoint.es>
2015-05-05 14:43:41 +0200

    Add tests for bug with repeated rows

--- test/BugTest.php
+++ test/BugTest.php
@@ -1,9 +1,9 @@
 <?php
 class BugTest extends DaffTestCase {
 	
-/**
- * Test rows are ordered correctly when a row is removed
- */
+	/**
+	* Test rows are ordered correctly when a row is removed
+	*/
 	public function testIssue11() {
 		$data1 = [
 			['Country','Capital'],
@@ -33,4 +33,45 @@ class BugTest extends DaffTestCase {
 			$this->assertEquals($row, $sortedRow);
 		}
 	}
+
+	public function testPatchWithRepeatedRows() {
+		$localTable = [
+			['a', 'b'],
+			[2, 2],
+			[2, 2],
+			[5, 6],
+		];
+
+		$remoteTable = [
+			['a', 'b'],
+			[5, 100],
+		];
+
+		$expectedTable = [
+			['a', 'b'],
+			[5, 100],
+		];
+
+		$local = new coopy_PhpTableView($localTable);
+		$remote = new coopy_PhpTableView($remoteTable);
+
+		$dataDiff = [];
+		$tableDiff = new coopy_PhpTableView($dataDiff);
+
+		$highlighter = new coopy_TableDiff(coopy_Coopy::compareTables($local, $remote)->align(), new coopy_CompareFlags());
+		$highlighter->hilite($tableDiff);
+
+		$patch = new coopy_HighlightPatch(
+			$local,
+			$tableDiff
+		);
+		$patch->apply();
+		$patchedData = $local->getData();
+
+		$this->assertEquals(2, count($patchedData));
+		for ($i = 0; $i < 2; $i++) {
+			$this->assertEmpty(array_diff($patchedData[i], $expectedTable[i]));
+		}
+	}
+
 }
